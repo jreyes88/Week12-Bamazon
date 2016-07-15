2e24680135e968badcda54bdcc441af757e26f15
Joey Reyes <jreyes88@gmail.com>
2016-07-15 08:38:27 -0500

    it's now updating quantities, just need to build the if-else to handle if there isn't enough stock

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,26 +1,24 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: '',
-	database: 'Bamazon'
-});
-connection.connect(function(err) {
-	if (err) throw err;
-});
+var connection = mysql.createConnection(
+	{
+		host: 'localhost',
+		port: 3306,
+		user: 'root',
+		password: '',
+		database: 'Bamazon'
+	}
+);
+connection.connect(
+	function(err) {
+		if (err) throw err;
+	}
+);
 
 var prompt = require('prompt');
 prompt.start();
 
-// function create() {
-// 	connection.query('INSERT INTO songs SET ?', [{ARTIST: "Lynyrd Skynyrd", TITLE:"Simple Man", GENRE: "Rock"}],
-// 		function(err, res) {});
-// };
-
 function read() {
 	connection.query('SELECT * FROM products', function(err, res) {
-
 		if(err) throw err;
 		for (var i = 0; i < res.length; i++) {
 			console.log("-------------------------------------------------------------------------------------------------------------------------------\n" + "ItemID: " + res[i].itemID + " || Product Name: " +  res[i].ProductName + " || Department Name: " + res[i].DepartmentName + " || Price: " + res[i].Price + " || Quantity: " + res[i].StockQuantity);
@@ -42,44 +40,38 @@ function promptUser(){
 				}
 			}
 		}, function (err, promptResult) {
-	    	// console.log("Product: " + promptResult.productChoiceItemID);
-	    	// console.log("Quantity: " + promptResult.productChoiceQuantity);
 	    	updateStock(promptResult.productChoiceItemID, promptResult.productChoiceQuantity);
 		}
 	);
 };
 read();
 
+function checkStockQuantity() {
+
+};
+
 function updateStock(productChoiceItemID, productChoiceQuantity) {
 	var newStockQuantity;
-	// console.log("productChoiceItemID: " + productChoiceItemID);
 	connection.query('SELECT * FROM products WHERE ?', {itemID:productChoiceItemID},
 		function(err, res) {
 			// console.log("results: " + res[0].StockQuantity);
 			var newStockQuantity = res[0].StockQuantity -= productChoiceQuantity;
-			// console.log("New Stock Quantity: " + newStockQuantity);
-			return newStockQuantity;
+			updateTableQuantity(productChoiceItemID, newStockQuantity);
 		});
-		updateTableQuantity(productChoiceItemID, newStockQuantity);
+			
 };
 
-function updateTableQuantity(updateProductItemID, updateStockQuantity) {
-	console.log("Product Item ID: " + updateProductItemID);
-	console.log("Updated Stock Quantity: " + updateStockQuantity);
-	connection.query("UPDATE products SET ? WHERE ?",
-		[
-			{
-				itemID: updateProductItemID
-			}, {
-				StockQuantity:  updateStockQuantity
-			}
-		],
-		function(err, res) {
-
-		}
-	);
+function updateTableQuantity(productChoiceItemID, newStockQuantity) {
+ 	connection.query("UPDATE products SET ? WHERE ?", [
+ 		{
+ 			StockQuantity: newStockQuantity
+ 		}, {
+ 			itemID: productChoiceItemID
+ 		}
+ 	], function(err, res) {})
 	read();
 };
+
 
 // function remove() {
 // 	connection.query("DELETE FROM songs WHERE ?", 
