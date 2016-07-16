fa8b8f12b8bcb8602720465a288c9f105ed4cc53
Joey Reyes <jreyes88@gmail.com>
2016-07-16 14:04:31 -0500

    Working on markdown file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Week12-Bamazon
+
+## Dependencies
+This application requires MySQL and prompt.
+`$ npm install mysql`
+`$ npm install prompt`
+
+Run the customer view with the command:
+`$ node BamazonCustomer.js`
+
+Run the manager view with the command:
+`$ node BamazonManager.js`
---
+++ SQL File/BamazonSQL.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM products
+LIMIT 0, 1000
+
+-- Date: 2016-07-16 13:49
+*/
+INSERT INTO `products` (`itemID`,`ProductName`,`DepartmentName`,`Price`,`StockQuantity`) VALUES (1,'Zelda','Video Games',20,9047);
+INSERT INTO `products` (`itemID`,`ProductName`,`DepartmentName`,`Price`,`StockQuantity`) VALUES (2,'Super Mario Brothers','Video Games',20,218);
+INSERT INTO `products` (`itemID`,`ProductName`,`DepartmentName`,`Price`,`StockQuantity`) VALUES (3,'Donkey Kong','Video Games',25,155);
+INSERT INTO `products` (`itemID`,`ProductName`,`DepartmentName`,`Price`,`StockQuantity`) VALUES (4,'Super Mario Kart 64','Video Games',15,0);
+INSERT INTO `products` (`itemID`,`ProductName`,`DepartmentName`,`Price`,`StockQuantity`) VALUES (5,'Pokemon GO','Video Games',1000,977);
+INSERT INTO `products` (`itemID`,`ProductName`,`DepartmentName`,`Price`,`StockQuantity`) VALUES (6,'Goldeneye 64','Video Games',40,84);
+INSERT INTO `products` (`itemID`,`ProductName`,`DepartmentName`,`Price`,`StockQuantity`) VALUES (7,'Washing Machine','Appliances',700,50);
+INSERT INTO `products` (`itemID`,`ProductName`,`DepartmentName`,`Price`,`StockQuantity`) VALUES (8,'Dryer','Appliances',850,65);
+INSERT INTO `products` (`itemID`,`ProductName`,`DepartmentName`,`Price`,`StockQuantity`) VALUES (9,'Dish Washer','Appliances',600,250);
+INSERT INTO `products` (`itemID`,`ProductName`,`DepartmentName`,`Price`,`StockQuantity`) VALUES (10,'Macbook Pro','Computers',1200,200);
+INSERT INTO `products` (`itemID`,`ProductName`,`DepartmentName`,`Price`,`StockQuantity`) VALUES (11,'Macbook Air','Computers',200,200);
+INSERT INTO `products` (`itemID`,`ProductName`,`DepartmentName`,`Price`,`StockQuantity`) VALUES (12,'Acer Laptop','Computers',50,6000);
